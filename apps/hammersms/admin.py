ec96b92c24a0041f63cdad4aec3537f15c9688cd
Jason <jason.oreilly@gmail.com>
2011-12-04 14:34:11 -0800

    RHOK weekend

---
+++ apps/hammersms/admin.py
@@ -0,0 +1,9 @@
+from hammersms.models import StopTime, Trip, Route, Stop
+from django.contrib import admin
+
+#admin.site.register(Agency)
+admin.site.register(Stop)
+admin.site.register(StopTime)
+admin.site.register(Trip)
+admin.site.register(Route)
+#admin.site.register(Calendar)
