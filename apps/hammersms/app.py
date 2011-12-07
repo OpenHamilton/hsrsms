c832641192f59c6067983b4ba42a336bcc30b165
Jason <jason.oreilly@gmail.com>
2011-12-07 08:05:18 -0800

    Following coding stadards specified in PEP 8

--- apps/hammersms/app.py
+++ apps/hammersms/app.py
@@ -1,6 +1,8 @@
 import re
-from datetime import time
+import time
+
 from rapidsms.apps.base import AppBase
+
 from hammersms.models import StopTime, Trip, Route, Stop
 
 class App(AppBase):
@@ -17,42 +19,55 @@ class App(AppBase):
    # or
    # HSR Next Rte 1A 10 min, Rte 2 15 min, Rte 4 17 min.
    # Use HSR 1234 1A for more King.  Use 1234 2 for more Barton
-   def handle(self, message):
-      HELP_MSG = 'Usage: \'HSR stopnumber\'\n Ex. \'HSR 3001\'\nor \'HSR 3001 5C\''
+    def handle(self, message):
+      HELP_MSG = 'Usage: \'HSR stopnumber (bus)\'\n Ex. \'HSR 3001\'\nor \'HSR 3001 5C\''
+      USER_ID = 'HSR' # prepended to text message
+      DISP_MAX = 3 # maximum bus stop times displayed
 
-      userid = 'HSR'
       msg = message.raw_text.strip().upper();
 
       # implement help functionality here 
-      if(msg == 'HELP' or msg == 'HELP.'):
-         message.respond(HELP_MSG)
-         return
-      
-      # parse sms into components.  If nothing is matched than the SMS is invalid
-      # match = re.match("^(?:" + userid + ")[ -_]*(?P<stop>\d{4})(?:[ \-_]+(?P<bus>[0-9A-Z]{1,4}))?$",msg,0) #Regex for 4 digits, use this with actual HSR data
-      match = re.match("^(?:" + userid + ")[ \-_]*(?P<stop>[^ ]+)(?:[ \-_]+(?P<bus>[0-9A-Z]{1,4}))?$",msg,0)        
-      
-      if(match == None):
-          message.respond('Invalid request. Text \'help\' for more options.')
-          return 
+    if(msg == 'HELP' or msg == 'HELP.'):
+        message.respond(HELP_MSG)
+        return
 
-      stop = match.group('stop')
-      bus = match.group('bus')
-      
-      #get the data relating to the stop# that the user has inputted
-      stoptimes = StopTime.objects.filter(stop__stop_id__contains=stop)
-                  
-      resp = ''
-      for item in stoptimes:
-		  resp += str(item)+ '\n'
-      	  		  
-      #resp = 'stop:' + stop
-      if(bus):
-          resp += ' bus:' + bus
- 
-      #TODO: implement lookup of bus schedule
-      
-     #right now, it's outputting all data for a stopid. It should get the first 3 times for a given stop #
-      message.respond(resp)
-      
-     
+      # parse sms into components.  If nothing is matched than the SMS is invalid
+#match = re.match("^(?:" + userid + ")[ -_]*(?P<stop>\d{4})(?:[ \-_]+(?P<bus>[0-9A-Z]{1,4}))?$",msg,0) #Regex for 4 digits, use this with actual HSR data
+    match = re.match("^(?:" + USER_ID + ")[ \-_]*(?P<stop>[^ ]+)(?:[ \-_]+(?P<bus>[0-9A-Z]{1,4}))?$",msg,0)
+
+    if(match == None):
+        message.respond('Invalid request. Text \'help\' for more options.')
+        return
+
+    stop = match.group('stop')
+    bus = match.group('bus')
+
+    # retrieve django filtered QuerySet objects
+    stoptimes_obj = StopTime.objects.filter(stop__stop_id__contains=stop)
+
+    print '\n' + str(stoptimes_obj[1]) + '\n'
+
+    # convert query objects into time structures
+    stoptimes = []
+    for stoptime_obj in stoptimes_obj:
+        time_tmp = time.strptime(str(stoptime_obj), '%H:%M:%S')
+        stoptimes.append(time_tmp)
+
+
+    current_time = time.localtime()
+
+    # Formulate response
+
+    resp = ''
+    for item in stoptimes:
+        resp += str(item)+ '\n'
+
+    #resp = 'stop:' + stop
+    if(bus):
+        resp += ' bus:' + bus
+
+    #TODO: implement lookup of bus schedule
+
+    #right now, it's outputting all data for a stopid. It should get the first 3 times for a given stop #
+    message.respond(resp)
+
