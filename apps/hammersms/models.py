ecbef7d07aec13afb1bff025fe510d751b0b6720
Jason <jason.oreilly@gmail.com>
2011-12-07 08:08:34 -0800

    Following coding standards specified in PEP 8

--- apps/hammersms/models.py
+++ apps/hammersms/models.py
@@ -2,34 +2,34 @@ from django.db import models
 
 class Route(models.Model):
 
-   def __unicode__(self):
-      return self.route_long_name
+    def __unicode__(self):
+        return self.route_long_name
 
-   route_long_name = models.CharField(max_length=40)
-   route_short_name = models.CharField(max_length=4, unique=True)
-   route_id = models.CharField(max_length=16, unique=True)
+    route_long_name = models.CharField(max_length=40)
+    route_short_name = models.CharField(max_length=4, unique=True)
+    route_id = models.CharField(max_length=16, unique=True)
 
 class Trip(models.Model):
-   
-   def __unicode__(self):
-      return str(self.trip_id)
 
-   route = models.ForeignKey(Route, to_field='route_id')
-   trip_id = models.CharField(max_length=16, unique=True)
+    def __unicode__(self):
+        return str(self.trip_id)
+
+    route = models.ForeignKey(Route, to_field='route_id')
+    trip_id = models.CharField(max_length=16, unique=True)
 
 class Stop(models.Model):
 
-   def __unicode__(self):
-      return self.stop_id
+    def __unicode__(self):
+        return self.stop_id
 
-   stop_id = models.CharField(max_length=16, unique=True)
+    stop_id = models.CharField(max_length=16, unique=True)
 
 class StopTime(models.Model):
 
-   def __unicode__(self):
-      return str(self.arrival_time)
+    def __unicode__(self):
+        return str(self.arrival_time)
 
-   arrival_time = models.TimeField() 
-   trip = models.ForeignKey(Trip, to_field='trip_id')
-   stop = models.ForeignKey(Stop, to_field='stop_id')
-   
+    arrival_time = models.TimeField()
+    trip = models.ForeignKey(Trip, to_field='trip_id')
+    stop = models.ForeignKey(Stop, to_field='stop_id')
+
