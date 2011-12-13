dbbd39f355b7e36d5e52b526b26186576257ff49
Jason <jason.oreilly@gmail.com>
2011-12-13 15:12:40 -0800

    Updated the models. ./manage.py loaddata is temporarily broken.

--- apps/hammersms/models.py
+++ apps/hammersms/models.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python
+# vim: ai ts=4 sts=4 et sw=4
+# encoding=utf-8
+
+
 from django.db import models
 
 class Route(models.Model):
@@ -16,6 +21,7 @@ class Trip(models.Model):
 
     route = models.ForeignKey(Route, to_field='route_id')
     trip_id = models.CharField(max_length=16, unique=True)
+    calendar = models.ForeignKey(Calendar, to_field='service_id')
 
 class Stop(models.Model):
 
@@ -32,4 +38,25 @@ class StopTime(models.Model):
     arrival_time = models.TimeField()
     trip = models.ForeignKey(Trip, to_field='trip_id')
     stop = models.ForeignKey(Stop, to_field='stop_id')
+
+class Calendar(models.Model):
+    '''Models the data found in the calendar.txt GTFS data file. Monday
+    through Friday are bools that tell if the stop times for a particular
+    trip are for that day. Stop times generally differ for Sat, Sun and
+    Holidays.
+    '''
+
+    def __unicode__(self):
+        return str(self.service_id)
+
+    service_id = models.CharField(max_length=16, unique=True)
+    monday = models.BooleanField()
+    tuesday = models.BooleanField()
+    wednesday = models.BooleanField()
+    thursday = models.BooleanField()
+    friday = models.BooleanField()
+    saturday = models.BooleanField()
+    sunday = models.BooleanField()
+    # start_date = YYYYMMDD
+    # end_date = YYYYMMDD
 
